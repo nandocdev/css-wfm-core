6c85037693a7ed757fec96d35c539ee2f6266aab
Fernando Castillo <ferncastillov@outlook.com>
2026-02-27 15:32:58 -0500

    chore(app): registrar providers de modulos

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -2,4 +2,13 @@
 
 return [
     App\Providers\AppServiceProvider::class,
+    App\Modules\Security\Providers\ModuleServiceProvider::class,
+    App\Modules\Core\Providers\ModuleServiceProvider::class,
+    App\Modules\Organization\Providers\ModuleServiceProvider::class,
+    App\Modules\Employee\Providers\ModuleServiceProvider::class,
+    App\Modules\Team\Providers\ModuleServiceProvider::class,
+    App\Modules\Schedule\Providers\ModuleServiceProvider::class,
+    App\Modules\Planning\Providers\ModuleServiceProvider::class,
+    App\Modules\Attendance\Providers\ModuleServiceProvider::class,
+    App\Modules\Workflow\Providers\ModuleServiceProvider::class,
 ];
